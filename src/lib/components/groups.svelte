4a3cec4d23877ca2c38d239b4ec1c5075f0d1da9
Nazar <113494366+NazCodeland@users.noreply.github.com>
2025-04-24 19:40:42 -0400

    add more to example

--- src/lib/components/groups.svelte
+++ src/lib/components/groups.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	import GroupUsers from '$lib/components/group-users.svelte';
 	import * as Menubar from '$lib/components/ui/menubar/index.js';
+	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
 
 	let { groups } = $props();
 </script>
 
 {#each groups as group (group)}
 	<Menubar.Sub>
-		<Menubar.SubTrigger>{group.title}</Menubar.SubTrigger>
+		<Menubar.SubTrigger class="p-0">
+			<Label class="flex w-full items-center gap-2 p-2">
+				<Checkbox class="pointer-events-none" />
+				{group.title}
+			</Label>
+		</Menubar.SubTrigger>
 		<GroupUsers groupUsers={group.users} />
 	</Menubar.Sub>
 {/each}
